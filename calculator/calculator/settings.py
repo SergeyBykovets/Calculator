b9b724e00ef11ad58ce58283fe43cf81612ff2b8
SergeyBykovets <bykovet@gmail.com>
2025-04-10 21:55:17 +0300

    Start

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'calculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'calculator_db',
+        'USER': 'root',
+        'PASSWORD': 'pro703190',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -118,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- calculator/main/views.py
+++ calculator/main/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django import forms
+from .models import CPU, GPU
 
 def main(request):
-    return render(request, 'main/main.html')
+    cpus = CPU.objects.all()  # Усі процесори з бази
+    gpus = GPU.objects.all()  # Усі відеокарти з бази
+
+    context = {
+        'cpus': cpus,
+        'gpus': gpus,
+    }
+    return render(request, 'main/main.html', context)
 
 def about(request):
     return render(request, 'main/about.html')
